c5b094785a13f55f9d1bc29479fd4a33e312fb34
yehia <yehia.elkashif@yahoo.com>
2020-04-01 23:25:36 +0200

    hello

---
+++ BubbleSort/src/BubbleSortTest.java
@@ -0,0 +1,33 @@
+public class BubbleSortTest {
+        public static void main(String[] args) {
+
+
+            int array[]={55,2,8,7,6,8,21};
+
+            for (int lastUnsortedIndex=array.length-1; lastUnsortedIndex>0; lastUnsortedIndex--){
+                for (int i=0; i<lastUnsortedIndex; i++){
+
+                    if (array[i]>array[i+1]){
+                        swap(array,i,i+1);
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+
+            }
+
+        }
+
+        public  static   void  swap(int array[],int i , int j){
+            if (array[i]==array[j]){
+                return;
+            }
+            int temp=array[i];
+            array[i]=array[j];
+            array[j]=temp;
+        }
+    }
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module data.structure.codes {
+}
