b50d591aa3e492af77a66c6f2edbf91a8d393289
yehia <yehia.elkashif@yahoo.com>
2020-04-17 17:02:41 +0200

    LinkedList

---
+++ dataStructure/src/LinkedList/EmployeeNode.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+public class EmployeeNode {
+
+    private  Employee employee;
+    private    EmployeeNode  next;
+
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    public  EmployeeNode (Employee employee){
+
+            this.employee=employee;
+
+    }
+
+    public String toString() {
+        return employee.toString();
+    }
+
+}
